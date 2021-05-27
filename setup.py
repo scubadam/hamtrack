cad8a73468f5d92883ed0751118e92f1a1218874
Dave Morris <scubadam@gmail.com>
2021-05-27 12:33:21 +0100

    fix missing comma in setup

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['gpiozero~=1.6.2']
+    install_requires=['gpiozero~=1.6.2'],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
